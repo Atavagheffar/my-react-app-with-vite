82363e6d8d72284c154e38a4ae9d2d8d83d471c9
Ata Ollah Vaghef Far <a.vagheffar@gmail.com>
2025-07-04 09:22:36 +0330

    add multi component + call them

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,22 @@
-export function Home() {
-  return <h1>hello ata :)</h1>;
+// import Home from "./Home.jsx";
+import Header from "./Header.jsx";
+import Maina from "./Maina.jsx";
+import Footer from "./Footer.jsx";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Maina />
+      <Footer />
+
+      <h1>hello ata :)</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, officiis?
+        Culpa cum tenetur tempora hic. Veniam at molestias, tempore voluptates
+        officiis velit aspernatur quos distinctio adipisci, quasi neque esse
+        cupiditate.
+      </p>
+    </div>
+  );
 }
---
+++ src/Maina.jsx
@@ -0,0 +1,3 @@
+export default function Maina() {
+  return <h3>this is main line</h3>;
+}
