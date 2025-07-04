f99d07d3a29e8c394161109c3697f67dafe385a0
Ata Ollah Vaghef Far <a.vagheffar@gmail.com>
2025-07-04 09:40:21 +0330

    use from {} and let

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,13 +4,15 @@ import Maina from "./Maina.jsx";
 import Footer from "./Footer.jsx";
 
 export default function Home() {
+  let name = "Ata Ollah";
+
   return (
     <div>
       <Header />
       <Maina />
       <Footer />
 
-      <h1>hello ata :)</h1>
+      <h1 id="name">hello {name} :)</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, officiis?
         Culpa cum tenetur tempora hic. Veniam at molestias, tempore voluptates
