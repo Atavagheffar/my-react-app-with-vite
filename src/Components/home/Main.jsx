2ca1fdcce0b7ac96d54c40665e695e1776d818ce
Ata Ollah Vaghef Far <a.vagheffar@gmail.com>
2025-07-05 09:52:13 +0330

    start working with state's

--- src/Components/home/Main.jsx
+++ src/Components/home/Main.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import CourseBox from "./CourseBox";
 import CourseList from "./CourseList";
+
 export default function Main() {
+  const [message, setMessage] = useState("not loaded");
+
   const clickHandler = () => {
-    console.log("clickkkkkkkkkkkkkkkkk :)");
+    // console.log("clickkkkkkkkkkkkkkkkk :)");
+    setMessage("loading ....");
   };
+  console.log(message);
+
   return (
     <div>
       <button onClick={clickHandler}>Click Me!</button>
+      <p>{message}</p>
     </div>
   );
 }
