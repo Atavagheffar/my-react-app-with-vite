cbdbb60b68cd0c945faf715e8bfda8764015f7ca
Ata Ollah Vaghef Far <a.vagheffar@gmail.com>
2025-07-07 21:47:46 +0330

    adding style

--- src/Components/home/CourseBox.jsx
+++ src/Components/home/CourseBox.jsx
@@ -1,11 +1,23 @@
 export default function CourseBox({ course }) {
   //   console.log(props);
 
+  const HeadStyle = {
+    backgroundColor: "red",
+    paddingBottom: "5rem",
+  };
+
   return (
     <div>
       <h2>{course?.title}</h2>
       {/* <p>{course?.details}</p> */}
-      <p>{course?.description}</p>
+      <p
+        className="course-description"
+        style={HeadStyle}
+        onMouseEnter={() => console.log("mouse enter")}
+        onMouseLeave={() => console.log("mouse leave")}
+      >
+        {course?.description}
+      </p>
       <span>time:10</span>
       <p>-*-*-*-*-*-*-*-</p>
     </div>
